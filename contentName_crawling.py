71b847be415c8997636d35bc4833d35a0bdba9f1
jujinhyeong <mhn03148@naver.com>
2022-10-18 07:38:01 +0900

    BeautifulSoup을 이용하여 웹크롤링을 한다. 게시판의 제목을 크롤링하여 '안전'태그와 '기타'태그로 나누어 dictionary를 생성한다.

--- contentName_crawling.py
+++ contentName_crawling.py
@@ -4,28 +4,30 @@ from bs4 import BeautifulSoup
 def crawling():
     url = 'https://see.knu.ac.kr/content/board/notice.html'
     response = requests.get(url)
-    content_name=[]
+
     content_dic = {}
 
     if response.status_code == 200:
         html = response.content
         soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')#한글 가져올때,깨질떄, utf-8로 설정해줘야 함
-
+        key_list = ['안전', '[학생]', '등록', '교직', '수업','']
         for i in range(1,35):
             title = soup.select_one('#content > div > div > div.board_list > div.board_body > table > tbody > tr:nth-child('+str(i)+') > td.left > a')
-            content_name.append(title.get_text().strip())
-            if ('안전' in title.get_text().strip()):
-                if content_dic.get("안전", -1) == -1:
-                    content_dic["안전"] = [title.get_text().strip()]
-                else:
-                    content_dic["안전"].append(title.get_text().strip())
-            else:
-                if content_dic.get("기타", -1) == -1:
-                    content_dic["기타"] = [title.get_text().strip()]
-                else:
-                    content_dic["기타"].append(title.get_text().strip())
 
-        print(content_dic)
+            for j in range(len(key_list)):
+                if key_list[j] in title.get_text().strip():
+                    if key_list[j] == '':
+                        if content_dic.get("기타", -1) == -1:
+                            content_dic["기타"] = [title.get_text().strip()]
+                        else:
+                            content_dic["기타"].append(title.get_text().strip())
+                    if content_dic.get(key_list[j], -1) == -1:
+                        content_dic[key_list[j]] = [title.get_text().strip()]
+                    else:
+                        content_dic[key_list[j]].append(title.get_text().strip())
+                    break
+
         return content_dic
     else:
         print(response.status_code)
+crawling()
