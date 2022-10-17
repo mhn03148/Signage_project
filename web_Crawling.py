2b625ef56ecb1f531fc3cc6d8f8604643c59e265
jujinhyeong <mhn03148@naver.com>
2022-10-18 05:39:34 +0900

    게시판 목록에 들어가서 content부분 캡쳐해서 저장하는 코드 그 후,공지사항 제목 크롤링

--- web_Crawling.py
+++ web_Crawling.py
@@ -1,11 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+#*[@id="content"]/div/div/div[2]/div[2]/table/tbody/tr[1]/td[2]/a
+#*[@id="content"]/div/div/div[2]/div[2]/table/tbody/tr[2]/td[2]/a
+#*[@id="content"]/div/div/div[2]/div[2]/table/tbody/tr[5]/td[2]/a
+#//*[@id="content"]/div/div/div[1]/table/tbody/tr[4]/td
 url = 'https://see.knu.ac.kr/content/board/notice.html'
 
 response = requests.get(url)
-
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('https://see.knu.ac.kr/content/board/notice.html')#웹페이지를 열어줌
+driver.implicitly_wait(3)#3초안에 웹페이지를 load 하면 바로 넘어가거나 3초를 기다림.
+driver.find_element(By.XPATH,r'//*[@id="content"]/div/div/div[2]/div[2]/table/tbody/tr[1]/td[2]/a').click()#해당 게시물을 클릭한다.
+driver.find_element(By.TAG_NAME,'body').send_keys(Keys.PAGE_DOWN)
+element = driver.find_element(By.CLASS_NAME,'contentview')
+element_png = element.screenshot_as_png
+with open('test.png','wb') as file:
+    file.write(element_png)
+driver.back()
+driver.find_element(By.XPATH,r'//*[@id="content"]/div/div/div[2]/div[2]/table/tbody/tr[2]/td[2]/a').click()
+element = driver.find_element(By.CLASS_NAME,'contentview')
+element_png1 = element.screenshot_as_png
+with open('test1.png','wb') as file:
+    file.write(element_png1)
 
 if response.status_code == 200:
     html = response.content
