15a147809b5bc330a6b13c40c9d772b2e6cfae24
jujinhyeong <mhn03148@naver.com>
2022-10-18 06:44:28 +0900

    BeautifulSoup을 이용하여 웹크롤링을 한다. 게시판의 제목을 크롤링하여 '안전'태그와 '기타'태그로 나누어 dictionary를 생성한다.

--- contentName_crawling.py
+++ contentName_crawling.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-from collections import  defaultdict
+
 
 url = 'https://see.knu.ac.kr/content/board/notice.html'
 response = requests.get(url)
 content_name=[]
-#content_dic = {}
-content_dic = defaultdict(list)
+content_dic = {}
+
 if response.status_code == 200:
     html = response.content
     soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')#한글 가져올때,깨질떄, utf-8로 설정해줘야 함
@@ -15,11 +15,16 @@ if response.status_code == 200:
         title = soup.select_one('#content > div > div > div.board_list > div.board_body > table > tbody > tr:nth-child('+str(i)+') > td.left > a')
         content_name.append(title.get_text().strip())
         if ('안전' in title.get_text().strip()):
-            content_dic["안전"].append(title.get_text().strip())
-            print(title.get_text().strip())
+            if content_dic.get("안전", -1) == -1:
+                content_dic["안전"] = [title.get_text().strip()]
+            else:
+                content_dic["안전"].append(title.get_text().strip())
         else:
-            content_dic["기타"].append(title.get_text().strip())
-    print(content_name)
+            if content_dic.get("기타", -1) == -1:
+                content_dic["기타"] = [title.get_text().strip()]
+            else:
+                content_dic["기타"].append(title.get_text().strip())
+
     print(content_dic)
 else :
     print(response.status_code)
